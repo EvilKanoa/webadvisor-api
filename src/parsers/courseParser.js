fad183b102359acdafb8333ce768c7cc5b4b5f1f
EvilKanoa <kanoa@kanoa.ca>
2018-11-05 17:41:43 -0500

    Started course parser

--- src/parsers/courseParser.js
+++ src/parsers/courseParser.js
@@ -2,8 +2,22 @@ const cheerio = require('cheerio');
 
 const parseCourses = async (html) => {
     const dom = cheerio.load(html);
+    const courseMap = new Map();
+};
+
+const insertRawCourse = (courseMap, rawCourse) => {
+    if (courseMap.has(rawCourse.code)) {
+        courseMap.get(rawCourse.code).sections.push(rawCourse.section);
+    } else {
+        const course = {
+            ...rawCourse,
+            sections: [rawCourse.section]
+        };
+        delete course.section;
+        courseMap.set(course.code, course);
+    }
 };
 
 module.exports = {
-    parseCourses,
+    parseCourses
 };
